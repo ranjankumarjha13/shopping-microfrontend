bcb4a77350403f48be77ed3aec07a2c7cb90e85f
ranjan <ranjan.smth4@gmail.com>
2026-09-19 00:52:10 +0530

    Added configuration changes in Host Application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shopping',
+    loadComponent: () =>
+      loadRemoteModule('shopping-mfe', './Products')
+        .then(m => m.ProductsComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
