a3e0e9c0101a3c5cddd489b9ba4801f4d1697595
ranjan <ranjan.smth4@gmail.com>
2026-09-21 01:30:26 +0530

    configuration changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,29 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/native-federation';
 
 const routes: Routes = [
+
   {
     path: 'shopping',
     loadComponent: () =>
       loadRemoteModule('shopping-mfe', './Products')
         .then(m => m.ProductsComponent)
+  },
+
+  {
+    path: 'cart',
+    loadComponent: () =>
+      loadRemoteModule('cart-microfrontend', './Component')
+        .then(m => m.App)
   }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
+
