c0ec36949d11652ab8c8572253e06ba6ba52206a
ranjan <ranjan.smth4@gmail.com>
2026-09-21 01:33:06 +0530

    changes

---
+++ federation.config.js
@@ -0,0 +1,27 @@
+const { withNativeFederation, shareAll } =
+  require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'ecommerce-host',
+
+  exposes: {
+    './Component': './src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+  ],
+
+});
